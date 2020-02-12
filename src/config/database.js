f91825b0d0523093471102d99f892ae4776ccb72
jairpro <jairedias@yahoo.com.br>
2020-02-11 21:29:59 -0300

    Update database.js
    removendo código comentado

--- src/config/database.js
+++ src/config/database.js
@@ -1,38 +1,3 @@
-/*
-const env = process.env.NODE_ENV || 'development';
-const config = require('./config.json')[env];
-
-module.exports = config;
-*/
-/*
-module.exports = {
-  dialect: 'mysql',
-  host: 'localhost',
-  // host: '127.0.0.1',
-  // port: '3306',
-  username: 'fastfeet',
-  password: 'fastfeet',
-  database: 'gostack_fastfeet_development',
-  // timezone: '-03:00',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
-  dialectOptions: {
-    // useUTC: false, // for reading from database
-    // useUTC: true, // for reading from database
-    dateStrings: true,
-    typeCast: true,
-  },
-  timezone: '-03:00', // for writing to database
-};
-*/
-/*
-import { development } from './config.json';
-
-export default development;
-*/
 const config = require('./config.json');
 
 const nodeEnv = process.env.NODE_ENV || 'development';
